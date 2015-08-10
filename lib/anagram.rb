2962152eb5dfd59889f84b40c1a6612fd8b51edf
supzann3 <susan.hehe01@gmail.com>
2015-08-10 13:21:00 -0400

    all done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  def initialize(word)
+    @word=word
+  end
+  def match(phrase)
+    # result=[]
+    phrase.select{|word| word.split(//).sort==@word.split(//).sort}
+    #why doesnt this work?
+  #     if word==@word
+  #       result << word
+  #
+  #     elsif word==@word.reverse
+  #       result << word
+  #     elsif (word.split(//) & @word.split(//))==word.split(//)
+  #       result<<word
+  #     # binding.pry
+  #     end
+  #   end
+  #   result
+  end
+end
